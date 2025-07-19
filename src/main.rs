c58a6af9a856108735f17f9e640d95a4df0131cf
Porrapat Petchdamrongskul <porrapat.cpe20@gmail.com>
2025-07-19 17:04:47 +0700

    add

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
 use serde::{Serialize, Deserialize};
 use serde_json;
 
+mod models;
+use models::{Epic, Story};
+
 #[derive(Serialize, Deserialize, Debug)]
 struct User {
     name: String,
@@ -8,32 +11,21 @@ struct User {
 }
 
 fn main() {
-    let user = User {
-        name: "เปา".to_string(),
-        age: 37,
-    };
 
-    // Serialize Rust -> JSON
-    let json = serde_json::to_string(&user).unwrap();
-    println!("{}", json); // {"name":"เปา","age":37}
+    let st = Epic::new(
+        "aaa".to_owned(),
+        "ddd".to_owned()
+    );
 
-    // Deserialize JSON -> Rust
-    let decoded: User = serde_json::from_str(&json).unwrap();
-    println!("{:?}", decoded);
+    let st_json = serde_json::to_string_pretty(&st).unwrap();
+    println!("{}", st_json);
+
+    let sto = Story::new(
+        "name".to_string(),
+        "description".to_string()
+    );
+
+    let sto_json = serde_json::to_string_pretty(&sto).unwrap();
+    println!("{}", sto_json);
 }
 
-// mod models;
-// use models::{Epic, Story};
-// fn main() {
-//     let st = Epic::new(
-//         "aaa".to_owned(),
-//         "ddd".to_owned()
-//     );
-    
-//     println!("{:?}", st.description);
-//     let sto = Story::new(
-//         "name".to_string(),
-//         "description".to_string()
-//     );
-//     println!("{:?}", sto.description);
-// }
--- src/models.rs
+++ src/models.rs
@@ -1,9 +1,14 @@
-pub enum Status {    Open,
+use serde::{Serialize, Deserialize};
+
+#[derive(Serialize, Deserialize, Debug)]
+pub enum Status {    
+    Open,
     InProgress,
     Resolved,
     Closed
 }
 
+#[derive(Serialize, Deserialize, Debug)]
 pub struct Epic {
     pub name: String,
     pub description: String,
@@ -21,6 +26,7 @@ impl Epic {
     }
 }
 
+#[derive(Serialize, Deserialize, Debug)]
 pub struct Story {
     pub name: String,
     pub description: String,
@@ -37,6 +43,7 @@ impl Story {
     }
 }
 
+#[derive(Serialize, Deserialize, Debug)]
 pub struct DBState {
     // This struct represents the entire db state which includes the last_item_id, epics, and stories
     // TODO: add fields (make sure the fields are public)
