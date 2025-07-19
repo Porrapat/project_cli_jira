42fee9087ef531c9bed4da8c646c350c4b961d7f
Porrapat Petchdamrongskul <porrapat.cpe20@gmail.com>
2025-07-19 15:26:05 +0700

    add more

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,25 @@
 mod models;
 
 fn main() {
-    let st = models::Epic {
-        id: 1,
-        name: "aaa".to_owned(),
-        description: "ddd".to_owned(),
-        status: models::Status::Closed
-    };
+    // let st = models::Epic {
+    //     // id: 1,
+    //     name: "aaa".to_owned(),
+    //     description: "ddd".to_owned(),
+    //     status: models::Status::Closed
+    // };
+    let st = models::Epic::new(
+        "aaa".to_owned(),
+        "ddd".to_owned()
+    );
+    
+    // {
+    //     // id: 1,
+    //     name: "aaa".to_owned(),
+    //     description: "ddd".to_owned(),
+    //     status: models::Status::Closed
+    // };
 
-    println!("{:?}", st);
+    println!("{:?}", st.name);
 }
 
 
--- src/models.rs
+++ src/models.rs
@@ -1,30 +1,31 @@
-#[derive(Debug)]
+// #[derive(Debug)]
 pub enum Status {
     // TODO: add fields (make sure the fields are public)
+    Open,
     InProgress,
+    Resolved,
     Closed
 }
-
-#[derive(Debug)]
+//  `status` can be `Open`, `InProgress`, `Resolved` or `Closed`. 
+// #[derive(Debug)]
 pub struct Epic {
     // TODO: add fields (make sure the fields are public)
-    pub id: u32,
+    // pub id: u32,
     pub name: String,
     pub description: String,
     pub status: Status
 }
-/*
-
-]"name": "Epic - Project 1",
-  "description": "This is Project 1 for the Bootcamp",
-  "status": "InProgress",
-   */
 
 impl Epic {
     pub fn new(name: String, description: String) -> Self {
-        todo!() // by default the status should be set to open and the stories should be an empty vector
+        Epic {
+            name,
+            description,
+            status: Status::InProgress
+        }
     }
 }
+// todo!() // by default the status should be set to open and the stories should be an empty vector
 
 pub struct Story {
     // TODO: add fields (make sure the fields are public)
