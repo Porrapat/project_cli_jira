783ffdfc9d58654ba890757730db3a4d4df860e2
Porrapat Petchdamrongskul <porrapat.cpe20@gmail.com>
2025-07-19 15:40:21 +0700

    add more

--- src/models.rs
+++ src/models.rs
@@ -21,23 +21,32 @@ impl Epic {
         Epic {
             name,
             description,
-            status: Status::InProgress
+            status: Status::Open
         }
     }
 }
 // todo!() // by default the status should be set to open and the stories should be an empty vector
 
 pub struct Story {
-    // TODO: add fields (make sure the fields are public)
+    pub name: String,
+    pub description: String,
+    pub status: Status,
 }
 
 impl Story {
     pub fn new(name: String, description: String) -> Self {
-        todo!() // by default the status should be set to open
+        Story {
+            name,
+            description,
+            status: Status::Open
+        }
     }
 }
 
 pub struct DBState {
     // This struct represents the entire db state which includes the last_item_id, epics, and stories
     // TODO: add fields (make sure the fields are public)
+    pub last_item_id: u32,
+    pub epics: Vec<Epic>,
+    pub stories: Vec<Story>
 }
