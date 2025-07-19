5da55f4f13b31be97b15e9f3742b46893e13d228
Porrapat Petchdamrongskul <porrapat.cpe20@gmail.com>
2025-07-19 15:16:15 +0700

    add more

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,21 @@
 mod models;
 
 fn main() {
-    println!("Hello, world!");
+    let st = models::Epic {
+        id: 1,
+        name: "aaa".to_owned(),
+        description: "ddd".to_owned(),
+        status: models::Status::Closed
+    };
+
+    println!("{:?}", st);
 }
+
+
+
+    // println!("Hello, world!");
+    // dbg!(models::Status::Closed);
+    // println!(models::Status::Closed);
+
+    // let result = models::Status::Closed;
+    // println!("{:?}", result);
--- src/models.rs
+++ src/models.rs
@@ -1,10 +1,24 @@
+#[derive(Debug)]
 pub enum Status {
     // TODO: add fields (make sure the fields are public)
+    InProgress,
+    Closed
 }
 
+#[derive(Debug)]
 pub struct Epic {
     // TODO: add fields (make sure the fields are public)
+    pub id: u32,
+    pub name: String,
+    pub description: String,
+    pub status: Status
 }
+/*
+
+]"name": "Epic - Project 1",
+  "description": "This is Project 1 for the Bootcamp",
+  "status": "InProgress",
+   */
 
 impl Epic {
     pub fn new(name: String, description: String) -> Self {
