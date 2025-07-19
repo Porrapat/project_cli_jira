f4b9836e28335303bf20854b60206c1247fd465e
Porrapat Petchdamrongskul <porrapat.cpe20@gmail.com>
2025-07-19 16:12:55 +0700

    add more

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,31 @@
 mod models;
-
+use models::{Epic, Story};
 fn main() {
+    let st = Epic::new(
+        "aaa".to_owned(),
+        "ddd".to_owned()
+    );
+    
+    let sto = Story::new(
+        "name".to_string(),
+        "description".to_string()
+    );
+    println!("{:?}", sto.description);
+}
+
+
     // let st = models::Epic {
     //     // id: 1,
     //     name: "aaa".to_owned(),
     //     description: "ddd".to_owned(),
     //     status: models::Status::Closed
     // };
-    let st = models::Epic::new(
-        "aaa".to_owned(),
-        "ddd".to_owned()
-    );
-    
     // {
     //     // id: 1,
     //     name: "aaa".to_owned(),
     //     description: "ddd".to_owned(),
     //     status: models::Status::Closed
     // };
-
-    println!("{:?}", st.name);
-}
-
-
-
     // println!("Hello, world!");
     // dbg!(models::Status::Closed);
     // println!(models::Status::Closed);
