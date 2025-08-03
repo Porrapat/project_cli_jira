d616e48bd915b202a8f49cf64cd958808f0ae56b
Porrapat Petchdamrongskul <porrapat.cpe20@gmail.com>
2025-08-03 12:39:02 +0700

    add

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,11 @@
 mod db;
 mod models;
 mod utility;
-
 use anyhow::Result;
+use crate::utility::{seeding_db, connecting_db};
 
 fn main() -> Result<()> {
-    utility::seeding_db("db_temp.json")?;
-    utility::connecting_db("db_temp.json");
+    seeding_db("db_temp.json")?;
+    connecting_db("db_temp.json");
     Ok(())
 }
