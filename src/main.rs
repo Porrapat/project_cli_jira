5ab2ff462b61b1e80308ed36c8ec38478bf09a75
Porrapat Petchdamrongskul <porrapat.cpe20@gmail.com>
2025-08-03 15:34:17 +0700

    add

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,14 @@ use anyhow::Result;
 use crate::utility::{seeding_db, connecting_db};
 
 fn main() -> Result<()> {
-    seeding_db("db_temp.json")?;
-    connecting_db("db_temp.json");
+    // seeding_db("db_temp.json")?;
+    // connecting_db("db_temp.json");
+    // clearscreen::clear().expect("failed to clear screen");
+
+    use ellipse::Ellipse;
+
+    let input = "🇩🇪🇬🇧🇮🇹🇫🇷";
+    println!("{}", &input.truncate_ellipse(2));
+    // assert_eq!(&input.truncate_ellipse(2), "🇩🇪🇬🇧...");
     Ok(())
 }
