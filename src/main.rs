dc1eb4e558d15af920fb657bd3d932e3eb9f21f0
Porrapat Petchdamrongskul <porrapat.cpe20@gmail.com>
2025-08-03 20:49:00 +0700

    Add more

--- src/main.rs
+++ src/main.rs
@@ -9,8 +9,6 @@ use crate::db::{JSONFileDatabase, Database, JiraDatabase};
 
 fn main() -> Result<()> {
     let db = JiraDatabase::new("db1.json".to_owned());
-    // db.read_db()?;
-    // let epic1 = 
 
     let epic1 = Epic {
         name: "Example Epic xxx".to_string(),
@@ -20,23 +18,25 @@ fn main() -> Result<()> {
     };
 
     db.create_epic(epic1);
-    // let db = JSONFileDatabase { file_path: "./data/db.json".to_owned() };
-    // // let db = JSONFileDatabase { file_path: filepath.to_owned() };
-    // match db.read_db() {
-    //     Ok(data) => println!("✅ Loaded DB: {:?}", data),
-    //     Err(err) => println!("❌ Failed to read DB: {}", err),
-    // }
 
-    // db.
-    // todo!("Not IMP");
-    // seeding_db("db_temp.json")?;
-    // connecting_db("db_temp.json");
-    // clearscreen::clear().expect("failed to clear screen");
-
-    // use ellipse::Ellipse;
-
-    // let input = "🇩🇪🇬🇧🇮🇹🇫🇷";
-    // println!("{}", &input.truncate_ellipse(2));
-    // // assert_eq!(&input.truncate_ellipse(2), "🇩🇪🇬🇧...");
     Ok(())
 }
+
+// let db = JSONFileDatabase { file_path: "./data/db.json".to_owned() };
+// // let db = JSONFileDatabase { file_path: filepath.to_owned() };
+// match db.read_db() {
+//     Ok(data) => println!("✅ Loaded DB: {:?}", data),
+//     Err(err) => println!("❌ Failed to read DB: {}", err),
+// }
+
+// db.
+// todo!("Not IMP");
+// seeding_db("db_temp.json")?;
+// connecting_db("db_temp.json");
+// clearscreen::clear().expect("failed to clear screen");
+
+// use ellipse::Ellipse;
+
+// let input = "🇩🇪🇬🇧🇮🇹🇫🇷";
+// println!("{}", &input.truncate_ellipse(2));
+// // assert_eq!(&input.truncate_ellipse(2), "🇩🇪🇬🇧...");
